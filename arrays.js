df551571d8c6a851043d92df55bfb15f5f6587ec
unknown <burete_bianca@yahoo.com>
2025-11-05 20:04:16 +0200

    objects and arrays

---
+++ arrays.js
@@ -0,0 +1,20 @@
+const arr = [1,2,3,4,5];
+
+// //console.log(arr);
+
+// //arr.push(6);
+
+// const popValues = arr.pop();
+
+// console.log(popValues);
+// console.log(arr);
+
+// for(let i=0 ; i < arr.length; i++)
+// {
+//     console.log(arr[i]);
+// }
+
+arr.forEach((e1,index) => {
+    console.log(e1,index);
+}
+)
