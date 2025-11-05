1557bb6113ffbb80dc4a77fea5fa89b044a9d809
unknown <burete_bianca@yahoo.com>
2025-11-05 20:05:10 +0200

    objects and arrays

--- arrays.js
+++ arrays.js
@@ -14,7 +14,22 @@ const arr = [1,2,3,4,5];
 //     console.log(arr[i]);
 // }
 
-arr.forEach((e1,index) => {
-    console.log(e1,index);
+// arr.forEach((e1,index) => {
+//     console.log(e1,index);
+// }
+// )
+
+// const mappedValues = arr.map((e1,index) => {
+// // console.log(e1,index);
+// return e1 *2;
+// })
+
+// console.log(mappedValues);
+
+for(e1 of arr){
+    console.log(e1);
 }
-)
+
+for(e1 in arr){
+    console.log(e1);
+}
